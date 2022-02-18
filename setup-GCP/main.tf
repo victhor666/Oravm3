e5f7b3363768e88670f14868278a366b563be242
Your Name <you@example.com>
2022-02-18 12:15:00 +0100

    correcciones

---
+++ setup-GCP/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.11.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.ProjectID
+  region  = var.Location
+  zone    = "${var.Location}-c"
+}
+
+############################
+#SERVIDOR ORACLE EN GCP    #
+############################
+module "gcp" {
+source                                = "../modulos/gcp"
+ProjectID                             = var.ProjectID
+Location                              = var.Location
+auto_create_subnetworks               = var.auto_create_subnetworks
+routing_mode                          = var.routing_mode
+auto_create_subnetworks               = var.auto_create_subnetworks
+delete_default_internet_gateway_routes= var.delete_default_internet_gateway_routes
+}
+
+                             
+                              
+               
+                          
+               
