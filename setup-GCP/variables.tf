4b990f03c0829920d3d19fb4274b0fdf2b6e2800
Your Name <you@example.com>
2022-02-18 12:21:40 +0100

    test

--- setup-GCP/variables.tf
+++ setup-GCP/variables.tf
@@ -1,6 +1,5 @@
 variable "ProjectID"  {}                           
 variable "Location"     {}                         
 variable "auto_create_subnetworks"{}               
-variable "routing_mode"             {}             
-variable "auto_create_subnetworks"    {}           
+variable "routing_mode"             {}                
 variable "delete_default_internet_gateway_routes"{}
